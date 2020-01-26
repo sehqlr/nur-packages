1e7a62547b1f218451042681b06b0cfe74e90321
sehqlr <hey@samhatfield.me>
2020-01-26 00:09:29 -0600

    Deleting those files again?

--- pkgs/upwork-desktop/shell.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-let upwork = import ./default.nix {};
-in
-
-(pkgs.buildFHSUserEnv {
-  name = "upwork-desktop-env";
-  targetPkgs = pkgs: [ upwork ];
-  runScript = "upwork";
-}).env
