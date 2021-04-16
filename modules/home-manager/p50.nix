12eefb0f8a1eab2d290779e40af0d8feb5aa4045
sehqlr <hey@samhatfield.me>
2021-04-15 20:51:32 -0500

    forgot to add file

---
+++ modules/home-manager/p50.nix
@@ -0,0 +1,50 @@
+{ config, pkgs, ... }:
+{
+  home.packages = with pkgs; [
+      chatterino2
+      discord
+      element-desktop
+      firefox
+      imv
+      paperwork
+      slack
+      spotify
+      zoom-us
+  ];
+
+  programs.obs-studio = {
+      enable = true;
+      plugins = [ pkgs.obs-wlrobs ];
+  };
+  
+  services.kanshi.profiles."3 monitors" = {
+    outputs = [
+      {
+        criteria = "DP-4";
+        position = "3840,0";
+      }
+      {
+        criteria = "DP-6";
+        position = "0,0";
+      }
+      {
+        criteria = "eDP-1";
+        position = "1920,0";
+      }
+    ];
+  };
+
+  wayland.windowManager.sway.config = {
+    assigns = {
+      "1: web" = [{ class = "^Firefox$"; }];
+      "3: slack" = [{ class = "^Slack$"; }];
+      "5: discord" = [{ class = "^discord$"; }];
+      "7: element" = [{ class = "^Element$"; }];
+    };
+    startup = [
+      { command = "${pkgs.slack}/bin/slack"; }
+      { command = "${pkgs.discord}/bin/discord"; }
+      { command = "${pkgs.element-desktop}/bin/element"; }
+    ];
+  };
+}
