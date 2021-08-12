5fee95b4397ddfb99b9443a93f9248ea72ca867d
sehqlr <hey@samhatfield.me>
2021-08-12 11:31:39 -0500

    P50 taskwarrior filters on HOME

--- modules/sam/p50.nix
+++ modules/sam/p50.nix
@@ -10,6 +10,8 @@
     xdg-desktop-portal-wlr
   ];
 
+  programs.taskwarrior.config.report.next.filter = "status:pending limit:page +HOME";
+
   services.kanshi.profiles = {
     undocked = {
       outputs = [{
